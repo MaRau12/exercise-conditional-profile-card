e783b2e6e3aad6a1936c340e9eb4aa289e72d032
MaRau12 <marthaelisabeth94@gmail.com>
2023-02-06 11:23:26 +0000

    done

--- src/js/app.js
+++ src/js/app.js
@@ -33,15 +33,48 @@ function render(variables = {}) {
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
-          <h2>Web Developer</h2>
-          <h3>Miami, USA</h3>
-          <ul class="position-right">
-            <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
-            <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/4geeksacademy"><i class="fab fa-instagram"></i></a></li>
+
+          <h1>${variables.name ? variables.name : "Your Name"} ${
+    variables.lastname ? variables.lastname : " "
+  }</h1>
+          <h2>${variables.role ? variables.role : "Please enter a role"}</h2>
+          <h3>${variables.city ? variables.city : ""} ${
+    variables.country ? variables.country : "where are you from?"
+  } </h3>
+
+
+
+
+          <ul class= ${
+            variables.socialMediaPosition == "position-right"
+              ? "position-right"
+              : "position-left"
+          } >
+            <li>
+              <a href="https://twitter.com/4geeksacademy">
+                <i class="fab fa-twitter"></i>
+              </a>
+            </li>
+            <li>
+              <a href="https://github.com/4geeksacademy">
+                <i class="fab fa-github"></i>
+              </a>
+            </li>
+            <li>
+              <a href="https://linkedin.com/4geeksacademy">
+                <i class="fab fa-linkedin"></i>
+              </a>
+            </li>
+            <li>
+              <a href="https://instagram.com/4geeksacademy">
+                <i class="fab fa-instagram"></i>
+              </a>
+            </li>
           </ul>
+
+
+
+
         </div>
     `;
 }
